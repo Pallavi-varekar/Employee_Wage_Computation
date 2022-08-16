bb5b417ad3515fe28b439d6e3f94007cd2ea1d2c
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-08-16 19:39:21 +0530

    added:month wage logic

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -5,6 +5,7 @@ public class Employee_Wage {
         System.out.println("Welcome to Employee Wage.");
         Random rand = new Random();
 
+
         int present = 1;
         int absent = 2;
         int part_time_present = 3;
@@ -21,20 +22,29 @@ public class Employee_Wage {
 
         int part_time_salary = 0;
 
+        int max_working_days = 20;
+        int monthly_salary = 0;
+        int monthly_parttime_salary = 0;
+
+
         switch (check) {
             case 1:
                 System.out.println("Employee is present.");
                 full_time_salary = wage_per_hr * full_day_hr;
+                monthly_salary =full_time_salary * max_working_days;
                 break;
             case 3:
                 System.out.println("Employee is present for part time.");
                 part_time_salary = wage_per_hr * part_time_hr;
+                monthly_parttime_salary = part_time_salary * max_working_days;
                 break;
             default:
                 System.out.println("Employee is absent.");
         }
         System.out.println("Daily employee wage: " + full_time_salary);
         System.out.println("Part time employee wage: " + part_time_salary);
+        System.out.println("total one month (assumed 20 days) salary: " + monthly_salary);
+        System.out.println("total one month (assumed 20 days) part time salary: " + monthly_parttime_salary);
     }
     }
 
