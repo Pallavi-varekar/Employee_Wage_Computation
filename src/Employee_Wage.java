25074c2fce7fffc059cc6ad3a7c14aee370fc810
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-08-16 19:14:40 +0530

    added:daily wage logic

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -3,19 +3,29 @@ import java.util.Random;
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage.");
-
-        Random rd = new Random();
+        Random rand = new Random();
 
         int present = 1;
         int absent = 2;
-        int check=rd.nextInt(3)+1;
+
+        int check=rand.nextInt(3)+1;
+
+        int wage_per_hr = 20;
+
+        int full_day_hr = 8;
+
+        int full_time_salary =0;
 
         if (check==present) {
             System.out.println("Employee is present.");
+            full_time_salary = wage_per_hr * full_day_hr;
         }
         else {
             System.out.println("Employee is absent.");
         }
+        System.out.println("Daily employee wage: " + full_time_salary);
     }
+
     }
+
 
