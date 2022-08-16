49ecae977e57ac780d750f3447bcf38ff5218cb3
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-08-16 19:00:15 +0530

    added:attendence logic

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage.");
+
+        Random rd = new Random();
+
+        int present = 1;
+        int absent = 2;
+        int check=rd.nextInt(3)+1;
+
+        if (check==present) {
+            System.out.println("Employee is present.");
+        }
+        else {
+            System.out.println("Employee is absent.");
+        }
     }
-}
+    }
+
